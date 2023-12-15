956c0aca0b1c5e4d3cad6730bf11bc414e7bc653
tigeryash <yashwanth.venkatesan@gmail.com>
2023-12-15 09:01:44 -0500

    correct syntax for preventing caching for api calls in nextjs

--- server/actions.ts
+++ server/actions.ts
@@ -3,7 +3,7 @@
 export async function getWeather(city: string, country: string) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}&units=metric`,{
         headers: {
-            'Cache-Control': 'no-store'
+            'cache': 'no-store'
         }
     
     })
@@ -14,7 +14,7 @@ export async function getWeather(city: string, country: string) {
 export async function getWeatherForCurrentLocation(city: string, country: string) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}&units=metric`, {
         headers: {
-            'Cache-Control': 'no-store'
+            'cache': 'no-store'
         }
     })
     const data = await response.json();
